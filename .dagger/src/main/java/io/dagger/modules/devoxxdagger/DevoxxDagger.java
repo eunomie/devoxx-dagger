4bb883c88da8171436960dbaa36158bcc9af2d89
Yves Brissaud <yves.brissaud@gmail.com>
2025-04-15 12:01:49 +0200

    add build-end and test
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- .dagger/src/main/java/io/dagger/modules/devoxxdagger/DevoxxDagger.java
+++ .dagger/src/main/java/io/dagger/modules/devoxxdagger/DevoxxDagger.java
@@ -6,15 +6,40 @@ import io.dagger.client.*;
 import io.dagger.module.annotation.DefaultPath;
 import io.dagger.module.annotation.Function;
 import io.dagger.module.annotation.Object;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 /** DevoxxDagger main object */
 @Object
 public class DevoxxDagger {
   @Function
-  public String isClean(@DefaultPath("/") Directory sources, Secret token)
+  public String signoff(@DefaultPath("/") Directory source, Secret token)
       throws ExecutionException, DaggerQueryException, InterruptedException {
-    dag().signoff(sources, token).isClean();
-    return "repository is clean";
+    dag().signoff(source, token).isClean();
+
+    var testOutput = test(source);
+    System.out.println(testOutput);
+
+    return "ready to signoff";
+  }
+
+  /** Return the result of running unit tests */
+  @Function
+  public String test(@DefaultPath("/") Directory source)
+      throws InterruptedException, ExecutionException, DaggerQueryException {
+    return this.buildEnv(source).withExec(List.of("npm", "run", "test:unit", "run")).stdout();
+  }
+
+  /** Build a ready-to-use development environment */
+  @Function
+  public Container buildEnv(@DefaultPath("/") Directory source) {
+    CacheVolume nodeCache = dag().cacheVolume("node");
+    return dag()
+        .container()
+        .from("node:21-slim")
+        .withDirectory("/src", source)
+        .withMountedCache("/root/.npm", nodeCache)
+        .withWorkdir("/src")
+        .withExec(List.of("npm", "install"));
   }
 }
