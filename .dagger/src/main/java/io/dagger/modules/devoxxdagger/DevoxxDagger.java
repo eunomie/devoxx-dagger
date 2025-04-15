0f66943612a68c1c5e1f3aa3763b75ce1d8fcdaf
Yves Brissaud <yves.brissaud@gmail.com>
2025-04-15 11:49:13 +0200

    is-clean
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- .dagger/src/main/java/io/dagger/modules/devoxxdagger/DevoxxDagger.java
+++ .dagger/src/main/java/io/dagger/modules/devoxxdagger/DevoxxDagger.java
@@ -2,33 +2,19 @@ package io.dagger.modules.devoxxdagger;
 
 import static io.dagger.client.Dagger.dag;
 
-import io.dagger.client.Container;
-import io.dagger.client.DaggerQueryException;
-import io.dagger.client.Directory;
+import io.dagger.client.*;
+import io.dagger.module.annotation.DefaultPath;
 import io.dagger.module.annotation.Function;
 import io.dagger.module.annotation.Object;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 /** DevoxxDagger main object */
 @Object
 public class DevoxxDagger {
-  /** Returns a container that echoes whatever string argument is provided */
   @Function
-  public Container containerEcho(String stringArg) {
-    return dag().container().from("alpine:latest").withExec(List.of("echo", stringArg));
-  }
-
-  /** Returns lines that match a pattern in the files of the provided Directory */
-  @Function
-  public String grepDir(Directory directoryArg, String pattern)
-      throws InterruptedException, ExecutionException, DaggerQueryException {
-    return dag()
-        .container()
-        .from("alpine:latest")
-        .withMountedDirectory("/mnt", directoryArg)
-        .withWorkdir("/mnt")
-        .withExec(List.of("grep", "-R", pattern, "."))
-        .stdout();
+  public String isClean(@DefaultPath("/") Directory sources, Secret token)
+      throws ExecutionException, DaggerQueryException, InterruptedException {
+    dag().signoff(sources, token).isClean();
+    return "repository is clean";
   }
 }
